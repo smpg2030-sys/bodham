61f9f6e19baa12cf02163523093f2f1f597831b0
Sai Madhav <madhav200320@gmail.com>
2026-02-14 19:39:18 +0530

    Refactor: remove SwipeLayout and update ResponsiveLayout in InnerApp

--- src/InnerApp.tsx
+++ src/InnerApp.tsx
@@ -1,20 +1,13 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import ResponsiveLayout from "./components/ResponsiveLayout";
 import FloatingSupport from "./components/FloatingSupport";
 import { HomeRefreshProvider } from "./context/HomeRefreshContext";
-import SwipeLayout from "./components/SwipeLayout";
 
 export default function InnerApp() {
-  const location = useLocation();
-
-  // List of paths that should be part of the horizontal swipe stack
-  const tabPaths = ["/", "/explore", "/messages", "/focus", "/profile"];
-  const isTabRoute = tabPaths.includes(location.pathname) || location.pathname.startsWith("/profile/");
-
   return (
     <HomeRefreshProvider>
       <ResponsiveLayout>
-        {isTabRoute ? <SwipeLayout /> : <Outlet />}
+        <Outlet />
         <FloatingSupport />
       </ResponsiveLayout>
     </HomeRefreshProvider>
